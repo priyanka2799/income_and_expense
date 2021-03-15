11a4fab3ad50057a5bc61b3a44b42de4b80b8e6c
priyanka2799 <walunj.priyanka@rediffmail.com>
2021-03-16 01:06:08 +0530

    dismiss and add button also the calculations

--- src/App.js
+++ src/App.js
@@ -2,62 +2,57 @@ import React from 'react';
 import './App.css';
 
 
-var totalAmount = 0;
-var positiveCurrency = 0;
-var negativeCurrency =0;
-
-
-
 const App = () => {
-    //const [searchTerm]
+  
     const list = [
-      // {"income": 'dont have for now',
-      //   "expenditure": "yeah everyone has to pay anyway",
-      //   "costs": "very high",
+      // {
+      //   amount: -300,
+      //   description: "stationary",
       // },
-      // {"income": ' have for now',
-      //   "expenditure": "yeah everyone has to pay anyway",
-      //   "costs": "very high",
-      // }
-      {
-        amount: -300,
-        description: "stationary",
-      },
-      {
-        amount: 110,
-        description: "pocket money",
-      },
+      // {
+      //   amount: 110,
+      //   description: "pocket money",
+      // },
     ] 
+
     const[List, setList] = React.useState(list);
+    const[totalAmount, setTotalAmount] = React.useState(0)
+    const[positiveCurrency, setPositiveCurrency] = React.useState(0)
+    const[negativeCurrency, setNegativeCurency] = React.useState(0)
 
     const handleList = (item1, item2) => (event) => {
       
       console.log(item1,item2);
-      const newList = [...list, {
+      const newList = [...List, {
         amount: item1,
         description: item2
       }]
+
       setList(newList);
+      setTotalAmount (totalAmount + parseInt(item1))
+      if(item1>0){ setPositiveCurrency(positiveCurrency + parseInt(item1))}
+      else if(item1<0){ setNegativeCurency(negativeCurrency - parseInt(item1))}
       event.preventDefault();
     }
 
-    const handleRemoveItem = (description) => {
-      const newList = list.filter((item) => item.description!= description);
+    const handleRemoveItem = (description, amount) => {
+      const newList = List.filter((item) => item.description!= description);
 
       setList(newList);
+
+      setTotalAmount (totalAmount - parseInt(amount))
+      if(amount>0){ setPositiveCurrency(positiveCurrency - parseInt(amount))}
+      else if(amount<0){ setNegativeCurency(negativeCurrency + parseInt(amount))}
     }
 
-    // const[totalAmount, setTotalAmount]= React.useState(totalAmount);
-    // const[positiveCurrency, setPositiveCurrency] = React.useState(setPositiveCurrency);
-    // const[negativeCurrency,setNegativeCurrency] = negativeCurrency
     return (
       <div >
         <div>Expense Card</div>
         <div>
-          Amount ({totalAmount})
+          Amount ${totalAmount}
           <div>
-            Income {positiveCurrency}
-            Expenditure {negativeCurrency}
+            Income ${positiveCurrency}
+            Expenditure ${negativeCurrency}
           </div>
           <div>
             <SearchBox handleList = {handleList}/>
@@ -66,17 +61,15 @@ const App = () => {
             <ListDisplay list = {List} onRemoveItem = {handleRemoveItem}/>
           </div>
         </div>
-
       </div>
-      
-    );
-  
+    ); 
 }
+
 const SearchBox = ({handleList}) => {
   return(
     <form>
       <label htmlFor ="amount">cost</label>
-      <input id = "amount" /> 
+      <input id = "amount" type = "number"/> 
       <label htmlFor = 'description'>
         Description:
       </label>
@@ -84,25 +77,25 @@ const SearchBox = ({handleList}) => {
       <button onClick = {(event) => handleList(document.getElementById("amount").value,document.getElementById("description").value )(event)}> Add</button>
     </form>
   );
-
 }
 
 const ListDisplay = ({list, onRemoveItem}) => (
-<div>
-  {console.log(list)}
+  <div>
+    {console.log(list)}
     {list.map(item=>( 
         <div key={item.description}>
           <span>{item.amount}</span>      
           <span>{item.description}</span>
           <span >
-            <button type="button" onClick = {() => {onRemoveItem(item.description)}}>
+            <button type="button" onClick = {() => {onRemoveItem(item.description, item.amount)}}>
               Dismiss
             </button>
           </span>
-          </div>   
-        )
-      )
+        </div>   
+        ))
     }
   </div>
 );
+
 export default App;
+
